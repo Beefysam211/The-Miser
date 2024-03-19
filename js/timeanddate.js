27fabd9f27612db12530ce9477517ad706e94dff
Beefysam211 <samarth.shammy.goel@gmail.com>
2024-03-20 03:24:19 +0530

    minified all the files

--- js/timeanddate.js
+++ js/timeanddate.js
@@ -1,7 +1,7 @@
 function display_c() {
-  mytime = setTimeout("display_ct()", 1e3);
+    mytime = setTimeout('display_ct()', 1e3)
 }
 function display_ct() {
-  var n = new Date();
-  (document.getElementById("ct").innerHTML = n), display_c();
+    var n = new Date()
+    ;(document.getElementById('ct').innerHTML = n), display_c()
 }
