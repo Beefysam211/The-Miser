1a4782b7b708c4a5d8f5ec0c5cee3b524e81b8b8
Beefysam211 <samarth.shammy.goel@gmail.com>
2024-03-20 06:47:35 +0530

    minified all the files

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,5 +1,3 @@
-function menuOnClick() {
-    document.getElementById('menu-bar').classList.toggle('change')
-    document.getElementById('nav').classList.toggle('change')
-    document.getElementById('menu-bg').classList.toggle('change-bg')
-}
+function menuOnClick(){document.getElementById('menu-bar').classList.toggle('change')
+document.getElementById('nav').classList.toggle('change')
+document.getElementById('menu-bg').classList.toggle('change-bg')}
